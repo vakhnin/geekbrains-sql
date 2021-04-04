8ed574d1b85aeadae9d95154d7b91518e96bad32
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-04-04 10:45:13 +0300

    lesson 05 tasks 04 "Операторы, фильтрация, сортировка и ограничение"

--- lesson05/lesson05_tasks01-05_filtration.sql
+++ lesson05/lesson05_tasks01-05_filtration.sql
@@ -133,6 +133,42 @@ ORDER BY
 	IF(value > 0, 0, 1), 
 	value;
 
+/* Задание # 4.
+ * (по желанию) Из таблицы users необходимо извлечь пользователей, 
+ * родившихся в августе и мае. 
+ * Месяцы заданы в виде списка английских названий (may, august)
+ */
+
+
+/* «Операторы, фильтрация, сортировка и ограничение». Для задания 4. */
+-- Подготавливаем БД для задания (из фала hw-5-data.sql)
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at datetime DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO
+  users (name, birthday_at)
+VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '2006-08-29');
+ 
+SELECT
+	name,
+	DATE_FORMAT(birthday_at, '%M') AS birthday_month
+FROM
+	users
+WHERE
+	DATE_FORMAT(birthday_at, '%M') IN ('May', 'August');
+
 
 
 
