571a9da23342d2221b22a9e2d64289a63ab1506e
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-04-04 10:36:04 +0300

    lesson 05 tasks 03 "Операторы, фильтрация, сортировка и ограничение"

--- lesson05/lesson05_tasks01-05_filtration.sql
+++ lesson05/lesson05_tasks01-05_filtration.sql
@@ -88,7 +88,50 @@ MODIFY updated_at DATETIME;
 
 
 -- проверяем
+SHOW CREATE TABLE users ;
 SELECT * FROM users;
+
+/* Задание # 3.
+ * В таблице складских запасов storehouses_products 
+ * в поле value могут встречаться самые разные цифры: 0, 
+ * если товар закончился и выше нуля, если на складе имеются запасы. 
+ * Необходимо отсортировать записи таким образом, 
+ * чтобы они выводились в порядке увеличения значения value. 
+ * Однако нулевые запасы должны выводиться в конце, после всех записей.
+ */
+
+
+/* «Операторы, фильтрация, сортировка и ограничение». Для задания 3. */
+-- Подготавливаем БД для задания (из фала hw-5-data.sql)
+DROP TABLE IF EXISTS storehouses_products;
+CREATE TABLE storehouses_products (
+  id SERIAL PRIMARY KEY,
+  storehouse_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Запасы на складе';
+
+INSERT INTO
+  storehouses_products (storehouse_id, product_id, value)
+VALUES
+  (1, 543, 0),
+  (1, 789, 2500),
+  (1, 3432, 0),
+  (1, 826, 30),
+  (1, 719, 500),
+  (1, 638, 1);
+ 
+
+-- выводим в описанном порядке
+ SELECT
+	value
+FROM
+	storehouses_products
+ORDER BY
+	IF(value > 0, 0, 1), 
+	value;
 
 
 
