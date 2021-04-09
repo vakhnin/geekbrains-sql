7cbddb9bad81118ea72116dbbb165c305377ed4e
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-04-09 13:03:32 +0300

    lesson 06 tasks 02

--- lesson06/lesson06_tasks01-04.sql
+++ lesson06/lesson06_tasks01-04.sql
@@ -26,15 +26,26 @@ GROUP BY from_user_id ORDER BY count(*) DESC LIMIT 1;
 -- (по желанию) 
 -- Пользователь, с которым больше всего общался (отправлял и получал сообщения) 
 -- с пользователем с id = 1
-SELECT
-	(SELECT
+SELECT (
+	SELECT
 		concat(first_name, ' ', last_name)
 	FROM
 		users
 	WHERE
-		id = uid) AS top_chat_user FROM (
-SELECT from_user_id AS uid FROM messages m WHERE  to_user_id=1 
-UNION ALL
-SELECT to_user_id AS uid FROM messages m WHERE  from_user_id=1) 
-AS t GROUP BY uid ORDER BY count(*) DESC LIMIT 1;
+		id = uid) AS top_chat_user 
+FROM (
+	SELECT from_user_id AS uid FROM messages m WHERE  to_user_id=1
+	UNION ALL
+	SELECT to_user_id AS uid FROM messages m WHERE  from_user_id=1
+	) AS t 
+GROUP BY uid ORDER BY count(*) DESC LIMIT 1;
+
+/* Задание # 2.
+ * Подсчитать общее количество лайков на посты, которые получили пользователи младше 18 лет.
+ */
+SELECT count(*) FROM posts_likes 
+WHERE post_id IN 
+	(SELECT id FROM posts WHERE user_id IN 
+		(SELECT user_id FROM profiles WHERE TIMESTAMPDIFF(YEAR, birthday , NOW()) < 18)) 
+	AND like_type
 
