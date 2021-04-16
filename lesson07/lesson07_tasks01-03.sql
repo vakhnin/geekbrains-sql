b6b86827192eebcce1530b1f989fa339cd81859e
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-04-16 15:21:24 +0300

    lesson 07 tasks 01-03. add solves with JOIN

--- lesson07/lesson07_tasks01-03.sql
+++ lesson07/lesson07_tasks01-03.sql
@@ -9,18 +9,32 @@ USE hw7;
  * которые осуществили хотя бы один заказ orders в интернет магазине.
 */
 
+-- Решение с подзапросом
 SELECT name FROM users 
 WHERE id 
 	IN (SELECT user_id FROM orders);
+
+
+-- Решение с JOIN
+SELECT name FROM users u 
+	JOIN orders o ON u.id = o.user_id
+	GROUP BY u.name 
 
 /* Задание # 2.
  * Выведите список товаров products и разделов catalogs, который соответствует товару.
 */
 
+
+-- Решение с подзапросом
 SELECT 
 	p.name, 
-	(SELECT c.name FROM catalogs AS c WHERE p.catalog_id=c.id) 
+	(SELECT c.name FROM catalogs AS c WHERE p.catalog_id=c.id) AS catalog
 FROM products AS p;
+
+-- Решение с JOIN
+SELECT p.name, c.name AS catalog 
+FROM products p
+	LEFT JOIN catalogs c ON p.catalog_id=c.id
 
 /* Задание # 2.
  * (по желанию) Пусть имеется таблица рейсов flights (id, from, to) 
@@ -58,8 +72,17 @@ INSERT INTO flights VALUES
 	(NULL, 'moscow', 'kazan');
 
 -- Выводим рейсы с русскими названиями
+-- Решение с подзапросом
 SELECT 
 	id, 
 	(SELECT name FROM cities WHERE label=flights.`from`) AS 'From',
 	(SELECT name FROM cities WHERE label=flights.`to`) AS 'To'
 FROM flights;
+
+-- Решение с JOIN
+SELECT f2.id, c2.name AS 'From', c3.name AS 'To' 
+	FROM flights f2 
+		JOIN cities c2 ON c2.label=f2.`from`
+		JOIN cities c3 ON c3.label=f2.`to`	
+	ORDER BY f2.id
+	
