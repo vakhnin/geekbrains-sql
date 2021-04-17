62d2b304429593d009e1001b4d878fc9ffc3ab44
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-04-17 11:50:02 +0300

    lesson 09 tasks 02 transactions

--- lesson09/lesson09_tasks01-04_transactions.sql
+++ lesson09/lesson09_tasks01-04_transactions.sql
@@ -14,3 +14,19 @@ INSERT INTO sample.users
 	SELECT * FROM shop.users WHERE id = 1;
 DELETE FROM shop.users WHERE id = 1;
 COMMIT;
+
+
+/* Задание # 2.
+ * Создайте представление, которое выводит название name товарной позиции 
+ * из таблицы products и соответствующее название каталога name из таблицы catalogs.
+*/
+
+USE shop;
+
+-- Создаем представление
+CREATE OR REPLACE VIEW view_product_catalog AS 
+	SELECT p2.name AS p_name, c2.name AS c_name FROM products p2 
+		JOIN catalogs c2 ON p2.catalog_id = c2.id
+	
+-- Проверяем
+SELECT * FROM view_product_catalog;
