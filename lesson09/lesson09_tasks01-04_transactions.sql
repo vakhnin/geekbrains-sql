d0e654e7a74bf8f6a1dd0c94f6490d86c6509d51
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-04-17 11:32:39 +0300

    lesson 09 tasks 01

---
+++ lesson09/lesson09_tasks01-04_transactions.sql
@@ -0,0 +1,16 @@
+/* Практическое задание #09 
+ * "Транзакции, переменные, представления". 
+ */
+
+/* Задание # 1.
+ * В базе данных shop и sample присутствуют одни и те же таблицы, 
+ * учебной базы данных. 
+ * Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. 
+ * Используйте транзакции.
+*/
+
+START TRANSACTION;
+INSERT INTO sample.users 
+	SELECT * FROM shop.users WHERE id = 1;
+DELETE FROM shop.users WHERE id = 1;
+COMMIT;
